9642c9b239d27625c0654566bb487e06d2afa1ba
vraykan <vitorandresaviandev@gmail.com>
2023-01-25 01:51:08 -0300

    feat: added lsp

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,24 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+	'tsserver',
+	'eslint',
+	'sumneko_lua',
+	'rust_analyzer',
+	'gopls'
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+lsp.setup()
--- lua/user/packer/init.lua
+++ lua/user/packer/init.lua
@@ -47,4 +47,12 @@ return require('packer').startup(function(use)
 	  }
   }
 
+  use {
+	  'nvim-tree/nvim-tree.lua',
+	  requires = {
+		  'nvim-tree/nvim-web-devicons', -- optional, for file icons
+	  },
+	  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+
 end)
