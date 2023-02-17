1142182a888331388ec5ccc6e878499ea21d7700
vitorsavian <vitorandresaviandev@gmail.com>
2023-02-17 17:13:37 -0300

    chore:added new null ls

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,13 +1,17 @@
 local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
-local sources = { 
-	formatting.goimports,
+local sources = {
+    formatting.goimports,
+    formatting.rustfmt,
+    diagnostics.credo,
+    diagnostics.cpplint,
+    formatting.mix,
+    formatting.clang_format
 }
 
 null_ls.setup({
-	sources = {
-		formatting.goimports,
-	}
+    sources,
 })
