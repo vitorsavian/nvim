2bdf69daece31dca63b4702a4b92e662c0a46545
vraykan <vitorandresaviandev@gmail.com>
2023-02-25 07:29:49 -0300

    chore: adding new configs for nvim-tree, lsp and init.lua

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,9 +1,37 @@
 -- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 0
+vim.g.loaded_netrwPlugin = 0
 
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
 -- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    view = {
+        width = 50,
+    },
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        show_on_open_dirs = true,
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
+        },
+    },
+    modified = {
+        enable = true,
+        show_on_dirs = true,
+        show_on_open_dirs = true,
+    },
+})
+
+local function open_nvim_tree()
+
+    -- always open the tree
+    require("nvim-tree.api").tree.open()
+end
+
+open_nvim_tree()
